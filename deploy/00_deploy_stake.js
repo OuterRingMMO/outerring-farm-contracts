444c884b5db7485bbf6fc94780d51565cf9c804c
Ibai-NexxyoLabs <ibai.castanon@nexxyolabs.com>
2022-01-19 17:40:30 +0100

    Configuring deploy file

--- deploy/00_deploy_stake.js
+++ deploy/00_deploy_stake.js
@@ -3,9 +3,18 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
     
+    const stakedToken = "";
+    const rewardToken = "";
+    const rewardPerBlock = "";
+    const startBlock = ;
+    const endBlock = ;
+    const lockUpDuration = ;
+    const withdrawFee = ;
+    const feeAddress = "";
+
     const stake = await deploy('Stake', {
         from: deployer,
-        args: [],
+        args: [stakedToken, rewardToken, startBlock, endBlock, lockUpDuration, withdrawFee, feeAddress],
         log: true,
     });
 
