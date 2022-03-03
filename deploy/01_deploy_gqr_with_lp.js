09e5c0e02f8b788f3fb38580a16b3373f283b2b1
Ibai-NexxyoLabs <ibai.castanon@nexxyolabs.com>
2022-03-03 18:12:04 +0100

    Deploy file for LP stakes

--- deploy/01_deploy_gqr_with_lp.js
+++ deploy/01_deploy_gqr_with_lp.js
@@ -2,14 +2,14 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
-    /*
-    let stakedTokenAddress = '';
-    let rewardTokenAddress = '';
-    let startBlock = ;
-    let endBlock = ;
-    let lockUpDuration = 7776000;
+    
+    let stakedTokenAddress = '0xAC1F5e57d53e9Ac3c092EB876e46C235df95672A';
+    let rewardTokenAddress = '0x227a3EF4d41d0215123f3197Faa087Bf71d2236a';
+    let startBlock = 15740550;
+    let endBlock = 18278000;
+    let lockUpDuration = 2592000;
     let withdrawFee = 500;
-    let feeAddress = '';
+    let feeAddress = '0xaAF6B6f4c3a20cae39A25fBcD9617822cd8bf1C7';
 
     const gqGalacticReserve = await deploy('GQGalacticReserve', {
         from: deployer,
@@ -40,7 +40,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
             withdrawFee,
             feeAddress
         ]
-    });*/
+    });
     
 };
 
