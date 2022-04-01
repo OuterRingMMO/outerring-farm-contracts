fdc0c8588a2753bfcd11085aebe4b06c9180b5d3
Ibai-NexxyoLabs <ibai.castanon@nexxyolabs.com>
2022-04-01 13:08:51 +0200

    Pre deploy

--- deploy/01_deploy_gqr_with_lp.js
+++ deploy/01_deploy_gqr_with_lp.js
@@ -1,15 +1,15 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
-    /*
+    
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
     
-    let stakedTokenAddress = '';
-    let rewardTokenAddress = '0x227a3EF4d41d0215123f3197Faa087Bf71d2236a';
-    let startBlock = 15912600;
-    let endBlock = 18520000;
+    let stakedTokenAddress = '0x6116D1eaD7CF92c5beD0Ae6A0e5d7e501149E102';
+    let rewardTokenAddress = '0x26D4e2f3943Fe603a8eCEB0A92019A2aAFF220ee';
+    let startBlock = 17837022;
+    let endBlock = 20429022;
     let lockUpDuration = 2592000;
     let withdrawFee = 500;
-    let feeAddress = '0xaAF6B6f4c3a20cae39A25fBcD9617822cd8bf1C7';
+    let feeAddress = '0x6080903C0017d0A6cf7C861910Cbc805Ee62740A';
 
     const gqGalacticReserve = await deploy('GQGalacticReserve', {
         from: deployer,
@@ -41,7 +41,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
             feeAddress
         ]
     });
-    */
+    
 };
 
 module.exports.tags = ['GQGalacticReserveWithLP'];
