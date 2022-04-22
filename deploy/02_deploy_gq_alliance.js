2a927a918d445b821249b0ef4cdfc0e38da1b836
Ibai-NexxyoLabs <ibai.castanon@nexxyolabs.com>
2022-04-22 09:27:42 +0200

    Added new deployments

--- deploy/02_deploy_gq_alliance.js
+++ deploy/02_deploy_gq_alliance.js
@@ -4,10 +4,10 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deployer } = await getNamedAccounts();
     
     let stakedTokenAddress = '0xF700D4c708C2be1463E355F337603183D20E0808';
-    let rewardToken1Address = '0x477bC8d23c634C154061869478bce96BE6045D12';
+    let rewardToken1Address = '0xD177E36377E71775d6f9956B3fDD0f02664C6996';
     let rewardToken2Address = '0x227a3ef4d41d0215123f3197faa087bf71d2236a';
-    let startBlock = 16655611;
-    let endBlock = 19207611;
+    let startBlock = 16711900;
+    let endBlock = 19263900;
     let lockUpDuration = 7776000;
     let withdrawFee = 300;
     let feeAddress = '0xaaf6b6f4c3a20cae39a25fbcd9617822cd8bf1c7 ';
