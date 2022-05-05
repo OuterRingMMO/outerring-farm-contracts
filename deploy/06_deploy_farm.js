822038ec347c7fa416d7975be430bfd7d1a40be8
Ibai-NexxyoLabs <ibai.castanon@nexxyolabs.com>
2022-05-05 12:43:51 +0200

    Deployed farming for IRON and COP

--- deploy/06_deploy_farm.js
+++ deploy/06_deploy_farm.js
@@ -3,17 +3,33 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    
+    const resource = '';
+    const resourcePerBlock = '';
+    const startBlock = ;
+
     const galacticFarming = await deploy('GalacticFarming', {
         from: deployer,
         args: [],
         log: true,
         proxy: {
             proxyContract: 'OpenZeppelinTransparentProxy',
+            execute: {
+                init: {
+                    methodName: "initialize",
+                    args: [
+                        resource, resourcePerBlock, startBlock
+                    ]
+                }
+            }
         }
     });
 
     console.log('GalacticFarming deployed at: ', galacticFarming.address);
-    
+    await sleep(10000);
     const galacticFarmingImplementation = await hre.deployments.get('GalacticFarming_Implementation');
     const galacticFarmingDeployed = await ethers.getContractAt('GalacticFarming', galacticFarmingImplementation.address);
     // Verification block
