33bfbb84fbab0a8ea81bb700e22e00a88aed668d
ibai-nexxyo <ibai.castanon@nexxyolabs.com>
2022-09-12 15:30:56 +0200

    Added new Pools

--- deploy/00_deploy_gqr.js
+++ deploy/00_deploy_gqr.js
@@ -5,12 +5,12 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    let stakedTokenAddress = '0xF700D4c708C2be1463E355F337603183D20E0808';
-    let rewardTokenAddress = '0x0EFCF1737b81CE89325b17EafAe2686A8aFE8bD4';
-    let startBlock = 18275534;
-    let endBlock = 23531534;
-    let lockUpDuration = 5184000;
-    let withdrawFee = 500;
+    let stakedTokenAddress = '0xea395DFaFEd39924988b475f2Ca7f4C72655203A';
+    let rewardTokenAddress = '0xF700D4c708C2be1463E355F337603183D20E0808';
+    let startBlock = 21268700;
+    let endBlock = 22996700;
+    let lockUpDuration = 0;
+    let withdrawFee = 0;
     let feeAddress = '0xaaf6b6f4c3a20cae39a25fbcd9617822cd8bf1c7';
 
     const gqGalacticReserve = await deploy('GQGalacticReserve', {
