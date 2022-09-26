00ca625cc5d641dd20e23787fc61c07e83f36473
ibai-nexxyo <ibai.castanon@nexxyolabs.com>
2022-09-26 12:14:41 +0200

    Deployed GQ-UIM

--- deploy/02_deploy_gq_alliance.js
+++ deploy/02_deploy_gq_alliance.js
@@ -2,13 +2,13 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
-
-    let stakedTokenAddress = '0xAC1F5e57d53e9Ac3c092EB876e46C235df95672A';
-    let rewardToken1Address = '0x227a3EF4d41d0215123f3197Faa087Bf71d2236a';
-    let rewardToken2Address = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56';
-    let startBlock = 18275534;
-    let endBlock = 20896334;
-    let lockUpDuration = 2592000;
+    
+    let stakedTokenAddress = '0xF700D4c708C2be1463E355F337603183D20E0808';
+    let rewardToken1Address = '0x1BB132D6039b81FaEdc524a30E52586b6Ca15f48';
+    let rewardToken2Address = '0xbD1945Cd85A2BE93a6475381c9F5EDF19407A921';
+    let startBlock = 21669700;
+    let endBlock = 23397700;
+    let lockUpDuration = 0;
     let withdrawFee = 500;
     let feeAddress = '0xaaf6b6f4c3a20cae39a25fbcd9617822cd8bf1c7 ';
 
@@ -17,8 +17,9 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         args: [],
         log: true,
         proxy: {
-            proxyContract: 'OpenZeppelinTransparentProxy'
-        }
+            proxyContract: "OpenZeppelinTransparentProxy",
+        },
+        waitConfirmations: 5
     });
     console.log('GQGalacticAlliance deployed at: ', gqGalacticAlliance.address);
     const GQGalacticAllianceImplementation = await hre.deployments.get('GQGalacticAlliance_Implementation');
