5f0d5456dc212e050fa27ee6f2ea13b0b5afbfc9
ibai-nexxyo <ibai.castanon@nexxyolabs.com>
2023-03-01 16:40:06 +0100

    Deployed GQStake 01/03/23
    New Pool for staking GQ/GQ has been deployed

--- deploy/00_deploy_gq_stake.js
+++ deploy/00_deploy_gq_stake.js
@@ -7,8 +7,8 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     let stakedTokenAddress = '0xF700D4c708C2be1463E355F337603183D20E0808';
     let rewardTokenAddress = '0xF700D4c708C2be1463E355F337603183D20E0808';
-    let startBlock = 18275534;
-    let endBlock = 23531534;
+    let startBlock = 26123534;
+    let endBlock = 31379534;
     let lockUpDuration = 5184000;
     let withdrawFee = 500;
     let feeAddress = '0xaaf6b6f4c3a20cae39a25fbcd9617822cd8bf1c7';
